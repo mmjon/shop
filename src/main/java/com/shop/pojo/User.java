deb11eba07474aae71534d5fb6841ffa7171abd8
mmjon <1684468484@qq.com>
2024-12-13 11:54:10 +0800

    User类提交

---
+++ src/main/java/com/shop/pojo/User.java
@@ -0,0 +1,15 @@
+package com.shop.pojo;
+
+import lombok.Data;
+
+/**
+ * @Author: Insight
+ * @Description: TODO
+ * @Date: 2024/12/13 11:50
+ * @Version: 1.0
+ */
+@Data
+public class User {
+    private Long id;
+    private String uname;
+}
